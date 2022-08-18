9961e7dcfe3ba8db7a27b661b9e144bdf2cfa0a6
Cy8rax <cybralex@gmail.com>
2022-08-19 00:28:11 +1100

    1

---
+++ Example005_TrickyPro/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+if(username == "Masha")
+{
+    Console.WriteLine("Hello, Masha, glad to see You!");
+}
+else
+{
+    Console.WriteLine("Hi, man!");
+    Console.WriteLine(username);
+}
