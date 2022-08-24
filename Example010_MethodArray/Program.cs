1220b0ef42461545e8d0f00e2abfd3c1c96a2d02
Cy8rax <cybralex@gmail.com>
2022-08-24 16:00:46 +1100

    Array Number in Data

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[]array={1,235,234,8756,234,6745,43,5754};
+
+int n=array.Length;
+
+int find = 5754;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine("Position of the Number");
+        Console.WriteLine(index);
+    }
+    index++;
+}
